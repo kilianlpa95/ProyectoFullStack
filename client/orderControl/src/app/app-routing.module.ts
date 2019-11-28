c5e7402af4c68d78ee62fe3e485fa6ee77aa20e2
kilian <kilianlpa95@gmail.com>
2019-11-28 16:03:29 +0000

    local storage

--- client/orderControl/src/app/app-routing.module.ts
+++ client/orderControl/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'local',
     pathMatch: 'full'
   },
   {
@@ -14,7 +14,12 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'local',
+    loadChildren: () => import('./local/local.module').then( m => m.LocalPageModule)
   }
+
 ];
 
 @NgModule({
---
+++ client/orderControl/src/app/services/storage.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+export interface Product {
+  id: number,
+  name: string,
+  description: string,
+  price: number
+}
+
+const PRODUCTS_KEY = 'keys';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StorageService {
+
+  constructor(private storage: Storage) { }
+
+  addProduct(product: Product): Promise<any> {
+    return this.storage.get(PRODUCTS_KEY).then((products: Product[]) => {
+      if (products){
+        products.push(product);
+        return this.storage.set(PRODUCTS_KEY, products);
+      } else {
+        return this.storage.set(PRODUCTS_KEY, [product]);
+      }
+    });
+  }
+
+  getProducts(): Promise<Product[]> {
+    return this.storage.get(PRODUCTS_KEY);
+  }
+
+  updateProduct(product: Product): Promise<any> {
+    return this.storage.get(PRODUCTS_KEY).then((products: Product[]) => {
+      if(!products || products.length === 0) {
+        return null;
+      }
+
+      let newProducts: Product[] = [];
+
+      for (let i of products){
+        if (i.id === product.id){
+          newProducts.push(product);
+        } else {
+          newProducts.push(i);
+        }
+      }
+
+      return this.storage.set(PRODUCTS_KEY, newProducts);
+    });
+  }
+
+  deleteProduct(id: number): Promise<Product[]> {
+    return this.storage.get(PRODUCTS_KEY).then((products: Product[]) => {
+      if(!products || products.length === 0) {
+        return null;
+      }
+
+      let item: Product[] = [];
+
+      for (let i of products){
+        if (i.id !== id){
+          item.push(i);
+        }
+      }
+
+      return this.storage.set(PRODUCTS_KEY, item);
+
+    });
+  }
+}
